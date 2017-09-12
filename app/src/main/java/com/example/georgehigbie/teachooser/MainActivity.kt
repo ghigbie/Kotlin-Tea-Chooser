02e4930b4ae0f80c6e1dd6d3944b4753121f7ecf
George Higbie <gehigbie@gmail.com>
2017-09-11 20:50:45 -0600

    Add method to decide tea

--- app/src/main/java/com/example/georgehigbie/teachooser/MainActivity.kt
+++ app/src/main/java/com/example/georgehigbie/teachooser/MainActivity.kt
@@ -2,11 +2,25 @@ package com.example.georgehigbie.teachooser
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    val teaList = arrayListOf("Oolong", "Matcha", "Sencha", "Hojicha", "Mugicha", "Irish Breakfast", "English Breakfast", "Darjeeling")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        decideBtn.setOnClickListener {
+            val random = Random()
+            val randomTea = random.nextInt(teaList.count())
+            displayText.text = teaList[randomTea]
+        }
+
+        addBtn.setOnClickListener {
+
+        }
     }
 }
